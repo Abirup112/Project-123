6bef4fe7e9d74ae905222a589d7597a324915ffa
Abirup112 <82591730+Abirup112@users.noreply.github.com>
2022-06-21 19:50:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference=0;
+leftWristX=0;
+rightWristX=0;
 function setup(){
     video=createCapture(VIDEO);
     video.size(550, 550);
@@ -7,14 +10,20 @@ function setup(){
     poseNet=ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-function draw(){
-    background("#00FF00");
-}
 function modelLoaded(){
     console.log('PoseNet is Initialized!');
 }
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
     }
+}
+function draw(){
+    background("#FFFF00");
+    fill('#FF0000');
+    textSize(difference);
+    text('Abhirup' ,50, 400);
 }
